75a6eb3ed28362e6ef25cb5e0c1265a22aa1c09a
Jose L. Velez <48886275+joselvelez@users.noreply.github.com>
2020-09-25 17:53:46 -0500

    added .add_edge method

--- graph.py
+++ graph.py
@@ -1,5 +1,4 @@
 from vertex import Vertex
-
 class Graph:
     def __init__(self, directed=False):
         self.directed = directed
@@ -8,6 +7,12 @@ class Graph:
     def add_vertex(self, vertex):
         print("Adding {vertex} to the railway Graph".format(vertex=vertex.value))
         self.graph_dict[vertex.value] = vertex
+
+    def add_edge(self, from_vertex, to_vertex, weight=0):
+        print("Adding edge from {from_v} to {to_v}".format(from_v=from_vertex.value, to_v=to_vertex.value))
+        self.graph_dict[from_vertex.value].add_edge(to_vertex.value, weight)
+        if not self.directed:
+            self.graph_dict[to_vertex.value].add_edge(from_vertex.value, weight)
 
 ''' Test / Debut '''
 # railway = Graph()
