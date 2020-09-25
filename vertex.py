2ef77ee988c69ec42abd960c744239af77e4ada3
Jose L. Velez <48886275+joselvelez@users.noreply.github.com>
2020-09-25 16:05:23 -0500

    added vertex class

---
+++ vertex.py
@@ -0,0 +1,20 @@
+class Vertex:
+    def __init__(self, value):
+        self.value = value
+        self.edges = {}
+
+    def add_edge(self, vertex):
+        print("Adding edge to {vertex}".format(vertex=vertex))
+        # A key in the Vertex instance’s edges dictionary represents a connection to that other vertex.
+        # For now, we can just set the value to be True.
+        self.edges[vertex] = True
+
+    def get_edges(self):
+        return list(self.edges.keys())
+
+''' Testing / Debug '''
+grand_central = Vertex('Grand Central Station')
+forty_second_street = Vertex('42nd Street Station')
+print(grand_central.get_edges())
+grand_central.add_edge(forty_second_street.value)
+print(grand_central.get_edges())
