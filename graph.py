f7ff9bcecc6b583de7a1ce45025aaea8c8639daf
Jose L. Velez <48886275+joselvelez@users.noreply.github.com>
2020-09-26 16:39:55 -0500

    completed graph class and .find_path method

--- graph.py
+++ graph.py
@@ -5,30 +5,41 @@ class Graph:
         self.graph_dict = {}
     
     def add_vertex(self, vertex):
-        print("Adding {vertex} to the railway Graph".format(vertex=vertex.value))
+        print("Adding {vertex} to the railway Graph"
+        .format(vertex=vertex.value))
         self.graph_dict[vertex.value] = vertex
 
     def add_edge(self, from_vertex, to_vertex, weight=0):
-        print("Adding edge from {from_v} to {to_v}".format(from_v=from_vertex.value, to_v=to_vertex.value))
+        print("Adding edge from {from_v} to {to_v}"
+        .format(from_v=from_vertex.value, to_v=to_vertex.value))
         self.graph_dict[from_vertex.value].add_edge(to_vertex.value, weight)
         if not self.directed:
             self.graph_dict[to_vertex.value].add_edge(from_vertex.value, weight)
 
     def find_path(self, start_vertex, end_vertex):
-        print("Searching for path between {start} and {end}".format(start=start_vertex, end=end_vertex))
+        print("\nSearching for path between {start} and {end}..."
+        .format(start=start_vertex, end=end_vertex))
         start = [start_vertex]
+        seen = {}
         while len(start) > 0:
             current_vertex = start.pop(0)
-            print("Visiting {current_v}".format(current_v=current_vertex))
+            seen[current_vertex] = True
+            print("\nVisiting {current_v}".format(current_v=current_vertex))
             if current_vertex == end_vertex:
-                return True
+                # return True
+                print("\nA connection exists between {start} and {end}! You have reached your destination."
+                .format(start=start_vertex, end=end_vertex))
             else:
                 vertex = self.graph_dict[current_vertex]
                 next_vertices = vertex.get_edges()
-                # print(start)
-                # print(next_vertices)
+                next_vertices = [vertex for vertex in next_vertices if vertex not in seen]
+                print("Start List: {start}".format(start=start))
+                print("List of Vertex Connections: {next_vertices}"
+                .format(next_vertices=next_vertices))
                 start.extend(next_vertices)
-        return False
+        # return False
+        print("\nNo connection seems to exist between {start} and {end}."
+        .format(start=start_vertex, end=end_vertex))
 
 ''' Test / Debut '''
 # railway = Graph()
--- script.py
+++ script.py
@@ -2,3 +2,25 @@ from vertex import Vertex
 from graph import Graph
 
 railway = Graph()
+
+callan = Vertex('callan')
+peel = Vertex('peel')
+ulfstead = Vertex('ulfstead')
+harwick = Vertex('harwick')
+
+railway.add_vertex(callan)
+railway.add_vertex(peel)
+railway.add_vertex(harwick)
+railway.add_vertex(ulfstead)
+
+railway.add_edge(peel, harwick)
+railway.add_edge(harwick, callan)
+railway.add_edge(callan, peel)
+
+peel_to_ulfstead_path_exists = railway.find_path('peel', 'ulfstead')
+harwick_to_peel_path_exists = railway.find_path('harwick', 'peel')
+
+# print("A path exists between peel and ulfstead:")
+# print(peel_to_ulfstead_path_exists)
+# print("A path exists between harwick and peel:")
+# print(harwick_to_peel_path_exists)
